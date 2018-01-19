94bbf5f11adb0fafcad6b9966cdbd1d4250e2362
Nootan Ghimire <nootan.ghimire@gmail.com>
2018-01-20 01:35:27 +0700

    Fix 500 on empty response from urbandictionary

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from bottle import route, run
+from bottle import route, run, HTTPResponse
 import requests
 
 @route('/<word>')
@@ -8,7 +8,13 @@ def home(word):
     word = word
     response = requests.get("http://api.urbandictionary.com/v0/define?term=" + word)
     answer = response.json()
+    
+    if not answer["list"]:
+        response.status = 404
+        return HTTPResponse(status=404, body= "Dafuq! Urban Dictionary has no idea what  <b>" + word +"</b> is! ")
+
     first = answer["list"][0]["definition"]
+    response.status = 200
     return first
 
 
