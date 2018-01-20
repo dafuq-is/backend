936101ce71477594264972c46065e0ba09b9b5bf
Nootan Ghimire <nootan.ghimire@gmail.com>
2018-01-20 18:11:39 +0700

    Refactor

--- src/sources/Exceptions/NoResultException.py
+++ src/sources/Exceptions/NoResultException.py
@@ -1,7 +1,2 @@
 class NoResultException(Exception):
-    def __init__(self, message):
-        self.messge = message
-
-    def __str__(self):
-        return self.message
-
+    pass
--- src/sources/UrbanDictionary.py
+++ src/sources/UrbanDictionary.py
@@ -1,5 +1,5 @@
 import requests
-#import NotFoundException
+from sources.Exceptions.NoResultException import NoResultException
 
 class UrbanDictionary(object):
     def __init__(self):
@@ -13,10 +13,11 @@ class UrbanDictionary(object):
             answer = result.json()
         except ValueError:
             # @todo: log this in future
-            raise NotFoundException('We could not find the term')
+            raise NoResultException('We could not find the term')
         if not answer["list"]:
-            raise  NotFoundException('We could not find the term')
+            raise  NoResultException('We could not find the term')
        
         return answer["list"][0]["definition"]
 
+#ud = UrbanDictionary
 
