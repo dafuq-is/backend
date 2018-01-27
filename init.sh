f121d879a0cb8a73bfa86751461e6bbd8644f734
Avasz <avasz@protonmail.com>
2018-01-27 19:25:30 +0545

    add both legacy and inotify filewatch methods

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 [[ -f .env ]] && source ./.env
 
 FILE='src/index.py'
 INIT="bash -c 'DAFUQ_IS_WORDNIK_API_KEY=${DAFUQ_IS_WORDNIK_API_KEY} exec -a dafuqis-backend python3 ${FILE} &'"
+checksum=$(md5sum $FILE)
 
 function boot
 {
@@ -12,15 +14,47 @@ function boot
 	sleep 1
 }
 
-function listen
+function inotify_watch 
 {
+
 	echo "$(date) - - Starting listening for changes...."
+
     while inotifywait -q -r -e modify src/
     do   		
 		clear
 		echo "$(date) - - Change detected, restarting...."
 		boot
     done
+}
+
+function legacy_watch
+{
+	echo -e "Notice: inotifywait not found in your system. 
+- Using legacy method to restart 
+- Can restart only if change is detected in index.py. 
+- If you want to enjoy feature of restarting on any changes in the directory, then install inotifywait in your system.
+	
+$(date) - - Started listening for changes...."
+	
+	while true
+	do
+		checksum_new=$(md5sum $FILE)
+		if [ "$checksum" != "$checksum_new" ]
+		then
+			checksum=$checksum_new
+			echo "$(date) - - Change detected in $FILE, restarting...."
+			boot
+		fi
+		sleep 1
+	done
+}
+
+
+function listen
+{
+
+	#[[ `which inotifywait` ]] && inotify_watch || legacy_watch
+	legacy_watch
 }
 
 # Start script
